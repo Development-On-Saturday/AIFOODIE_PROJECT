1d21a59365b09e7fd7596a2e0a41ee63e9aa59ab
jh-ubuntu <ljh415@gmail.com>
2020-12-22 21:59:53 +0900

    update run script

--- run.py
+++ run.py
@@ -1,11 +1,38 @@
 #!usr/bin/python
 
+from __future__ import print_function
+import argparse
 import os, sys
 
-cmds = ['./django_dev', 'pip install -r requirements.txt', 'python manage.py migrate', 'python manage.py runserver']
+def to_bool(x):
+    if x.lower() in ['true','t']:
+        return True
+    elif x.lower() in ['false','f']:
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Bool 값을 넣으세요')
 
-for cmd in cmds:
-    if 'django' in cmd:
-        os.chdir(cmd)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', type=to_bool, default=False,
+                        metavar='runserver',
+                        help='if you want to run server only'
+                        )
+
+    args = parser.parse_args()
+
+
+    cmds = ['./django_dev', 'pip install -r requirements.txt', 'python manage.py migrate', 'python manage.py runserver']
+
+    if args.r :
+        os.chdir('./django_dev')
+        os.system('python manage.py runserver')
     else :
-        os.system(cmd)
+        for cmd in cmds:
+            if 'django' in cmd:
+                os.chdir(cmd)
+            else :
+                os.system(cmd)
+
+if __name__ == '__main__':
+    main()
